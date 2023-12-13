c928d81aa739c51dc492de9b9e16a0b2fc1df1a0
Gabriel Lucca <gabriel.santana60@aluno.senai.br>
2023-12-13 15:06:49 -0300

    Event API Content Moderator Implementado

---
+++ webapi.event+/Contexts/Event_Context.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore;
+using webapi.event_.Domains;
+
+namespace webapi.event_.Contexts
+{
+    /// <summary>
+    /// Classe de contexto que faz referências as tabelas e define string de conexão
+    /// </summary>
+    public class Event_Context : DbContext
+    {
+        public DbSet<TiposUsuario> TiposUsuario { get; set; }
+
+        public DbSet<Usuario> Usuario { get; set; }
+
+        public DbSet<TiposEvento> TiposEvento { get; set; }
+
+        public DbSet<Evento> Evento { get; set; }
+
+        public DbSet<ComentariosEvento> ComentariosEvento{ get; set; }
+
+        public DbSet<Instituicao> Instituicao { get; set; }
+
+        public DbSet<PresencasEvento> PresencasEvento { get; set; }
+
+        /// <summary>
+        /// Define as opções de construção do banco
+        /// </summary>
+        /// <param name="optionsBuilder">Objeto com as configurações definidas</param>
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            // String de conexão Local SSMS 
+
+           // optionsBuilder.UseSqlServer("Server=NOTE02-S14; Database=event_gabriel;  User Id=sa; Pwd=Senai@134; TrustServerCertificate=True;");
+
+            // string de conexão SQL Database - Azure 
+            base.OnConfiguring(optionsBuilder);
+
+            optionsBuilder.UseSqlServer("Server=tcp:eventgabriel-server.database.windows.net,1433;Initial Catalog=eventGabrielDatabase;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30; User Id = eventgabriel-server; Pwd=Senai@134;"); 
+
+        }
+    }
+}
---
+++ webapi.event+/Controllers/ComentariosEventoController.cs
@@ -0,0 +1,145 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using webapi.event_.Domains;
+using webapi.event_.Repositories;
+using webapi.event_.Interfaces;
+using Microsoft.Azure.CognitiveServices.ContentModerator;
+using System.Text;
+
+namespace webapi.event_.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Produces("application/json")]
+    public class ComentariosEventoController : ControllerBase
+    {
+
+
+        // acesso aos métodos do repositório 
+        ComentariosEventoRepository comentario = new ComentariosEventoRepository();
+
+        //  Armazena dados da API externa (IA - Azure)
+        private readonly ContentModeratorClient _contentModeratorClient;
+
+        public ComentariosEventoController(ContentModeratorClient contentModeratorClient)
+        {
+            _contentModeratorClient = contentModeratorClient;
+        }
+
+        [HttpPost("ComentarioIA")]
+        public async Task<IActionResult> PostIA(ComentariosEvento comentariosEvento)
+        {
+            try
+            {
+                // se a descrição do comentário não for passado no objeto 
+                if (string.IsNullOrEmpty(comentariosEvento.Descricao))
+                {
+                    return BadRequest("O Texto a ser analisado não pode ser Vazio !!");
+                }
+                // converte a string(descrição do comentário em um MemoryStream
+                using var stream = new MemoryStream(Encoding.UTF8.GetBytes(comentariosEvento.Descricao));
+
+                // realiza a moderação do conteúdo(descrição do comentário) 
+                var moderationResult = await _contentModeratorClient.TextModeration
+                    .ScreenTextAsync("text/plain", stream, "por", false, false, null, true);
+
+                // se existir termos ofensivos
+                if (moderationResult.Terms != null)
+                {
+                    //atribuir false para "Exibe" e também cadastra o Comentário
+                    comentariosEvento.Exibe = false;
+
+                    comentario.Cadastrar(comentariosEvento);
+                }
+                else
+                {
+                    comentariosEvento.Exibe = true;
+
+                    comentario.Cadastrar(comentariosEvento);
+                }
+                return StatusCode(201, comentariosEvento);
+            }
+            catch (Exception e)
+            {
+
+                return BadRequest(e.Message);
+            }
+        }
+
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            try
+            {
+                return Ok(comentario.Listar());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpGet("ListarSomenteExibe")]
+        public IActionResult GetIA()
+        {
+            try
+            {
+                return Ok(comentario.ListarSomenteExibe());
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+
+
+        [HttpGet("BuscarPorIdUsuario/{id}")]
+        public IActionResult GetByIdUser(Guid idUsuario, Guid idEvento)
+        {
+            try
+            {
+                return Ok(comentario.BuscarPorIdUsuario(idUsuario, idEvento));
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Post(ComentariosEvento novoComentario)
+        {
+            try
+            {
+                comentario.Cadastrar(novoComentario);
+                return StatusCode(201, novoComentario);
+            }
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(Guid id)
+        {
+            try
+            {
+                comentario.Deletar(id);
+                return NoContent();
+            }
+
+            catch (Exception e)
+            {
+                return BadRequest(e.Message);
+            }
+        }
+
+
+    }
+}
